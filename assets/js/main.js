6f1dbb2cf0add5c3c4910b2c7af98a21b044d794
mertakkan <makkan19@ku.edu.tr>
2023-07-09 21:52:59 +0300

    Search Bar, Coingecko API, JS Filter added

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const search = document.getElementById("search");
+  const list = document.getElementById("crypto-list");
+  let cryptoData = [];
+
+  fetch("https://api.coingecko.com/api/v3/coins/list")
+    .then((response) => response.json())
+    .then((data) => {
+      cryptoData = data;
+      displayCryptos(cryptoData);
+    });
+
+  search.addEventListener("input", (event) => {
+    const searchText = event.target.value.toLowerCase();
+    const filteredData = cryptoData.filter((crypto) =>
+      crypto.name.toLowerCase().includes(searchText)
+    );
+    displayCryptos(filteredData);
+  });
+
+  function displayCryptos(data) {
+    list.innerHTML = "";
+    data.forEach((crypto) => {
+      const cryptoElement = document.createElement("div");
+      cryptoElement.classList.add("crypto-card");
+      cryptoElement.innerText = crypto.name;
+      list.appendChild(cryptoElement);
+    });
+  }
+});
